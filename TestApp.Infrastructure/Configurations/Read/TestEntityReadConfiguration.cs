2f6ce05574247e9a7fabb491d16581a0a4ed2b14
ASKurgan <infopret@yandex.ru>
2025-04-04 15:55:00 +0300

    Add another commits

---
+++ TestApp.Infrastructure/Configurations/Read/TestEntityReadConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestApp.Domain.Entities;
+using TestApp.Infrastructure.ReadModels;
+
+namespace TestApp.Infrastructure.Configurations.Read
+{
+    public class TestEntityReadConfiguration : IEntityTypeConfiguration<TestEntityReadModel>
+    {
+        public void Configure(EntityTypeBuilder<TestEntityReadModel> builder)
+        {
+            builder.ToTable("test_entity");
+            
+            builder.HasKey(t => t.Id);
+        }
+    }
+}
---
+++ TestApp.Infrastructure/ReadModels/TestEntityReadModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp.Infrastructure.ReadModels
+{
+    public class TestEntityReadModel
+    {
+        public long Id { get; init; }
+        public int Code { get; init; }
+        public string Value { get; init; } = string.Empty;
+    }
+}
